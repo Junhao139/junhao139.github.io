602542aeaabaa9f903f54d7f9264a97ad9395987
#JunHao <56094517+Junhao139@users.noreply.github.com>
2020-09-12 01:29:55 +0200

    Update functions.js

--- functions.js
+++ functions.js
@@ -3,6 +3,6 @@ var innerHeight = window.innerHeight;
 var innerWidth = window.innerWidth;
 
 if (innerWidth / innerHeight < 1.333) {
-    window.location.replace("https://raw.githubusercontent.com/Junhao139/junhao139.github.io/master/m/index.html");
+    window.location.replace("./m/");
 }
 
