30f8185f81cbe8377f0c5ce398eb8b896b9839ed
#JunHao <56094517+Junhao139@users.noreply.github.com>
2020-09-12 00:27:01 +0200

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,6 +1,6 @@
 var innerHeight = window.innerHeight;
 var innerWidth = window.innerWidth;
 
-if (innerHeight / innerWidth < 1.333) {
+if (innerWidth / innerHeight < 1.333) {
     window.location.replace("");
 }
