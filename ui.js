0dbdb0f9bc4bb78ed85fdd04deab217a4b00d6ed
#JunHao <56094517+Junhao139@users.noreply.github.com>
2020-09-12 00:24:54 +0200

    Create ui.js

---
+++ ui.js
@@ -0,0 +1,30 @@
+
+
+function topBar() {
+    var innerWidth = window.innerWidth;
+    document.getElementById("navBar").style.width = (innerWidth - 30) + "px";
+	if (document.documentElement.scrollTop > 10) {
+		document.getElementById("navBar").style.background = "rgba(253, 253, 253, 1)";
+		document.getElementById("navBar").style.boxShadow = "0 0 15px -7.5px rgba(32, 48, 48, 0.75)";
+		document.getElementById("navBar_title").style.color = "#000";
+	} else {
+		document.getElementById("navBar").style.background = "rgba(253, 253, 253, 0)";
+		document.getElementById("navBar").style.boxShadow = "0 0 15px -7.5px rgba(0, 0, 0, 0)";
+
+		document.getElementById("navBar_title").style.color = "#fff";
+	}
+}
+
+var innerHeight = window.innerHeight;
+var nowTime = new Date;
+function onload() {
+	document.getElementById("Time").innerHTML = nowTime.getFullYear() + " 年 " + (nowTime.getMonth() + 1) + " 月 " + nowTime.getDate() + " 日 " + nowTime.getHours() + " : " + nowTime.getMinutes();
+	document.getElementById("main").style.height = innerHeight + "px";
+	if ((window.innerWidth / window.innerHeight) < 1.5) {
+		document.getElementById("main").style.backgroundSize = "auto 100%";
+	} else {
+		document.getElementById("main").style.backgroundSize = "100% auto";
+	}
+}
+
+setInterval(function(){topBar(), onload()}, 1);
