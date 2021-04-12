14ed15e21a57e7f4fd137e46619e3e9884d689d9
#JunHao <56094517+Junhao139@users.noreply.github.com>
2021-04-12 19:56:29 +0200

    Update commonBlogScript.js

--- resources/commonBlogScript.js
+++ resources/commonBlogScript.js
@@ -2,11 +2,11 @@ function customPageMargin() {
     var width = window.innerWidth;
     var height = window.innerHeight;
 
-    if ((width / height) < 1.0) {
-        document.getElementsByTagName("body")[0].style.margin = "120px 10% 20px 10%";
-    } else if ((width / height) < 0.6) {
-        document.getElementsByTagName("body")[0].style.margin = "120px 20% 20px 20%";
-    } else {
+    if ((width / height) < 0.6) {
         document.getElementsByTagName("body")[0].style.margin = "120px 10px 20px 10px";
+    } else if ((width / height) < 1.0) {
+        document.getElementsByTagName("body")[0].style.margin = "120px 10% 20px 10%";
+    } else {
+        document.getElementsByTagName("body")[0].style.margin = "120px 20% 20px 20%";
     }
 }
