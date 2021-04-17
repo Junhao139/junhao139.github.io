7704d29d0979ef9642673d7fe5cc03ff7d5ecc24
#JunHao <56094517+Junhao139@users.noreply.github.com>
2021-04-17 11:29:30 +0200

    some good things

--- readerScript.js
+++ readerScript.js
@@ -1,5 +1,6 @@
 window.onload = function() {
     convertSource();
+    setInterval(function() {customPageMargin();}, 50);
 }
 
 /* CREATE IFRAME */
@@ -38,5 +39,19 @@ function convertSource() {
         /* OUTPUT */
         var parentElem = document.getElementById("pageContent");
         parentElem.innerHTML = markdownOutput;
+    }
+}
+
+/* CUSTOM PAGE MARGIN TO FIX PAGE */
+function customPageMargin() {
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+
+    if ((width / height) < 0.6) {
+        document.getElementsByTagName("body")[0].style.margin = "120px 10px 20px 10px";
+    } else if ((width / height) < 1.0) {
+        document.getElementsByTagName("body")[0].style.margin = "120px 10% 20px 10%";
+    } else {
+        document.getElementsByTagName("body")[0].style.margin = "120px 20% 20px 20%";
     }
 }
