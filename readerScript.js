ee8b4d96478a486149989844e790d7d84019b3f8
#JunHao <56094517+Junhao139@users.noreply.github.com>
2021-04-18 16:05:06 +0200

    plus de service !

--- readerScript.js
+++ readerScript.js
@@ -1,6 +1,17 @@
+/* GLOBAL VARIABLES */
+var contentIsLoaded = false;
+
 window.onload = function() {
-    convertSource();
+    convertSource("local");
     setInterval(function() {customPageMargin();}, 50);
+    var contentLoadIntervalID = setInterval(function() {
+        if (!contentIsLoaded) {
+            askForChangingSource(true);
+        } else {
+            askForChangingSource(false);
+            clearInterval(contentLoadIntervalID);
+        }
+    }, 4000);
 }
 
 /* GET CONVERTED HTML STRING */
@@ -10,24 +21,58 @@ function getMarkdownTexts(originalString) {
     return markdownOutput;
 }
 
+/* LOAD CONTENT WITH CALLED SOURCE */
+function getMarkdownContentWithSource(sourceType, fileName) {
+    var directedURL;
+    switch (sourceType) {
+    case "local":
+        directedURL = "https://blog.zminutes.com/texts/";
+        break;
+    case "gitRaw":
+        directedURL = "https://raw.githubusercontent.com/Junhao139/junhao139.github.io/master/texts/";
+        break;
+    case "replit":
+        directedURL = "https://PersonalBlogContainer.junhaoliu.repl.co/texts/";
+        break;
+    }
+
+    directedURL += fileName;
+
+    /* GET CONTENTS THEN PUSH THEM */
+    $.get(
+        directedURL,
+        function (callback, status) {
+            if (!contentIsLoaded) {
+                document.getElementById("pageContent").innerHTML = getMarkdownTexts(callback);
+                contentIsLoaded = true;
+            }
+        }
+    );
+}
+
 /* MAIN CONVERTER FUNCTION */
-function convertSource() {
+function convertSource(sourceType) {
     /* GET URL PARAMETER */
     var wholeURL = window.location.search;
     var urlParameters = new URLSearchParams(wholeURL);
     var directingFile = urlParameters.get("cnt");
 
     /* CONVERT TO HTML */
-    var directingURL_local = "https://blog.zminutes.com/texts/" + directingFile + ".md";
-    var directingURL_gitRaw = "https://raw.githubusercontent.com/Junhao139/junhao139.github.io/master/texts/" + directingFile + ".md";
-    var usedSource = directingURL_local;
+    getMarkdownContentWithSource(sourceType, directingFile + ".md");
+}
 
-    $.get(
-        usedSource,
-        function (callback, status) {
-            document.getElementById("pageContent").innerHTML = getMarkdownTexts(callback);
-        }
-    )
+/* ASK FOR IF THE CONTENT IS NOT LOAD AFTER A WHILE */
+function askForChangingSource(option) {
+    var elem = document.getElementById("wantToChangeSource");
+
+    switch (option) {
+    case true:
+        elem.style.display = "block";
+        break;
+    case false:
+        elem.style.display = "none";
+        break;
+    }
 }
 
 /* CUSTOM PAGE MARGIN TO FIX PAGE */
