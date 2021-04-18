b72c85f33ad774584bcbe6afcb0a2605ca5802d4
#JunHao <56094517+Junhao139@users.noreply.github.com>
2021-04-18 11:20:07 +0200

    interesting things

--- readerScript.js
+++ readerScript.js
@@ -35,11 +35,13 @@ function customPageMargin() {
     var width = window.innerWidth;
     var height = window.innerHeight;
 
+    var container = document.getElementById("pageContent");
+
     if ((width / height) < 0.6) {
-        document.getElementsByTagName("body")[0].style.margin = "120px 10px 20px 10px";
+        container.style.margin = "120px 10px 20px 10px";
     } else if ((width / height) < 1.0) {
-        document.getElementsByTagName("body")[0].style.margin = "120px 10% 20px 10%";
+        container.style.margin = "120px 10% 20px 10%";
     } else {
-        document.getElementsByTagName("body")[0].style.margin = "120px 20% 20px 20%";
+        container.style.margin = "120px 20% 20px 20%";
     }
 }
