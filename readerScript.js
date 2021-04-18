43ea504ec36bbccb63fb64e4d2abff6afcbd30d4
#JunHao <56094517+Junhao139@users.noreply.github.com>
2021-04-18 10:20:18 +0200

    change code structure

--- readerScript.js
+++ readerScript.js
@@ -3,21 +3,10 @@ window.onload = function() {
     setInterval(function() {customPageMargin();}, 50);
 }
 
-/* CREATE IFRAME */
-function createIframe(url) {
-    var iframe = document.createElement("iframe");
-    iframe.setAttribute("src", url);
-    iframe.setAttribute("id", "GET_CNT_IFRAME_ELEM");
-    return iframe;
-}
-
 /* GET CONVERTED HTML STRING */
-function getMarkdownTexts(iframeElement) {
-    var elmnt = iframeElement.contentDocument || iframeElement.contentWindow.document;
-    var bodyElement = elmnt.getElementsByTagName("pre")[0].innerHTML;
-
+function getMarkdownTexts(originalString) {
     var MarkDownConverter = new showdown.Converter();
-    var markdownOutput = MarkDownConverter.makeHtml(bodyElement);
+    var markdownOutput = MarkDownConverter.makeHtml(originalString);
     return markdownOutput;
 }
 
@@ -29,17 +18,15 @@ function convertSource() {
     var directingFile = urlParameters.get("cnt");
 
     /* CONVERT TO HTML */
-    var outputIframe = createIframe("https://blog.zminutes.com/texts/" + directingFile + ".md");
-    outputIframe.style.display = "none";
-    document.getElementsByTagName("body")[0].appendChild(outputIframe);
-
-    outputIframe.onload = function () {
-        var markdownOutput = getMarkdownTexts(outputIframe);
-
-        /* OUTPUT */
-        var parentElem = document.getElementById("pageContent");
-        parentElem.innerHTML = markdownOutput;
-    }
+    var directingURL_local = "https://blog.zminutes.com/texts/" + directingFile + ".md";
+    var directingURL_gitRaw = "https://raw.githubusercontent.com/Junhao139/junhao139.github.io/master/texts/" + directingFile + ".md";
+    
+    $.get(
+        directingURL_local,
+        function (callback, status) {
+            document.getElementById("pageContent").innerHTML = getMarkdownTexts(callback);
+        }
+    )
 }
 
 /* CUSTOM PAGE MARGIN TO FIX PAGE */
