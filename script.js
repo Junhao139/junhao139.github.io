10c9203b8ef00e5429a4fc919c33118e5fd277b1
#JunHao <56094517+Junhao139@users.noreply.github.com>
2021-05-22 22:23:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,16 +7,9 @@ function repartAllHrefs() {
     for (var i = 0, sums = 0; i < elems.length; ++i) {
         if (elems[i].getAttribute("zm-cnt-src") != null) {
             elems[i].setAttribute("id", "CNTLINK_" + sums);
-            elems[i].setAttribute("onclick", "jumpToThatPage('CNTLINK_" + sums + "');");
+            elems[i].setAttribute("href", "./reader.html?cnt=" + elems[i].getAttribute("zm-cnt-src"));
 
             ++sums;
         }
     }
-}
-
-function jumpToThatPage( divElementID ) {
-    var element = document.getElementById(divElementID);
-    var elemSrc = element.getAttribute("zm-cnt-src");
-
-    window.location = "./reader.html?cnt=" + elemSrc;
 }
