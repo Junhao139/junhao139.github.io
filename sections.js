e0193726ec0a661a2707900e456511ff6e8bdd48
#JunHao <88624404+nuevo-aroma-kaori@users.noreply.github.com>
2021-10-29 19:01:26 +0200

    new post

---
+++ sections.js
@@ -0,0 +1,254 @@
+{
+    "identifier" : "ZMINUTES_BLOG_PROP",
+    "type_directories" : [
+        {
+            "text_type_name" : "chinese_blogs",
+            "text_directory" : "zhBlogs",
+            "text_type_show_name" : "写写小文"
+        },
+        {
+            "text_type_name" : "chinese_stories",
+            "text_directory" : "zhStories",
+            "text_type_show_name" : "写写小文（故事）"
+        },
+        {
+            "text_type_name" : "english_stories",
+            "text_directory" : "enStories",
+            "text_type_show_name" : "Write some things"
+        },
+        {
+            "text_type_name" : "stories",
+            "text_directory" : "stories",
+            "text_type_show_name" : "Writings & stories"
+        },
+        {
+            "text_type_name" : "translation_works",
+            "text_directory" : "stories",
+            "text_type_show_name" : "有感情的翻译机"
+        },
+        {
+            "text_type_name" : "amic_retrobat",
+            "text_directory" : "amicRetrobat",
+            "text_type_show_name" : "《L'amic retrobat》（重逢）"
+        }
+    ],
+    "all_posts" : [
+        {
+            "show_name" : "2021 年 4 月 11 日记",
+            "type" : "chinese_blogs",
+            "file_id" : "2021-4-11",
+            "post_type" : "txt",
+            "post_time" : {
+                "UNIX" : 1618099820
+            }
+        },
+        {
+            "show_name" : "《墙》（2021 年 4 月 17 日上传）",
+            "type" : "chinese_stories",
+            "file_id" : "2021-4-17-QIANG",
+            "post_type" : "txt",
+            "post_time" : {
+                "UNIX" : 1618670402
+            }
+        },
+        {
+            "show_name" : "2021 年 4 月 17 日记",
+            "type" : "chinese_blogs",
+            "file_id" : "2021-4-17",
+            "post_type" : "txt",
+            "post_time" : {
+                "UNIX" : 1618672820
+            }
+        },
+        {
+            "show_name" : "2021 年 4 月 18 日 —— 闲聊",
+            "type" : "chinese_blogs",
+            "file_id" : "2021-4-18",
+            "post_type" : "txt",
+            "post_time" : {
+                "UNIX" : 1618778418
+            }
+        },
+        {
+            "show_name" : "2021 年 4 月 22 日 —— 特别待遇恐惧症",
+            "type" : "chinese_blogs",
+            "file_id" : "2021-4-22-TEBIEDAIYU",
+            "post_type" : "txt",
+            "post_time" : {
+                "UNIX" : 1619083789
+            }
+        },
+        {
+            "show_name" : "2021 年 4 月 25 日 —— 早睡是真的！",
+            "type" : "chinese_blogs",
+            "file_id" : "2021-4-25",
+            "post_type" : "txt",
+            "post_time" : {
+                "UNIX" : 1619376558
+            }
+        },
+        {
+            "show_name" : "2021 年 4 月 28 日 —— 诗句分享（西）",
+            "type" : "chinese_blogs",
+            "file_id" : "2021-4-28-POEM",
+            "post_type" : "txt",
+            "post_time" : {
+                "UNIX" : 1619631976
+            }
+        },
+        {
+            "show_name" : "2021 年 5 月 22 日 —— 《来自时间的声音》",
+            "type" : "chinese_stories",
+            "file_id" : "2021-5-22-TIME_S_SOUND",
+            "post_type" : "txt",
+            "post_time" : {
+                "UNIX" : 1621642820
+            }
+        },
+        {
+            "show_name" : "2021 年 6 月 16 日 —— 《L'amic retrobat》翻译计划",
+            "type" : "chinese_blogs",
+            "file_id" : "2021-6-16-AMIC_RETROBAT",
+            "post_type" : "txt",
+            "post_time" : {
+                "UNIX" : 1623853038
+            }
+        },
+        {
+            "show_name" : "2021 年 8 月 19 日 —— 嗨",
+            "type" : "chinese_blogs",
+            "file_id" : "2021-8-19",
+            "post_type" : "txt",
+            "post_time" : {
+                "UNIX" : 1629335789
+            }
+        },
+        {
+            "show_name" : "2021 年 9 月 4 日记",
+            "type" : "chinese_blogs",
+            "file_id" : "2021-9-4",
+            "post_type" : "txt",
+            "post_time" : {
+                "UNIX" : 1630772298
+            }
+        },
+        {
+            "show_name" : "Surprise! (13-Apr-2021)",
+            "type" : "english_stories",
+            "file_id" : "2021-4-13",
+            "post_type" : "txt",
+            "post_time" : {
+                "UNIX" : 1618330698
+            }
+        },
+        {
+            "show_name" : "One last time (10-May-2021)",
+            "type" : "english_stories",
+            "file_id" : "2021-5-10",
+            "post_type" : "txt",
+            "post_time" : {
+                "UNIX" : 1620663498
+            }
+        },
+        {
+            "show_name" : "Ennuyeux. (19-Apr-2021)",
+            "type" : "stories",
+            "file_id" : "2021-4-19-MARIE_ET_GABRIEL",
+            "post_type" : "txt",
+            "post_time" : {
+                "UNIX" : 1618849098
+            }
+        },
+        {
+            "show_name" : "Sant Jordi: redacción（圣乔治节：参选作文）(21-Apr-2021)",
+            "type" : "stories",
+            "file_id" : "2021-4-21-SANT-JORDI",
+            "post_type" : "txt",
+            "post_time" : {
+                "UNIX" : 1619021898
+            }
+        },
+        {
+            "show_name" : "El Azar Y La Vida (18-Oct-2021)",
+            "type" : "stories",
+            "file_id" : "2021-10-18-El-azar-y-la-vida",
+            "post_type" : "txt",
+            "post_time" : {
+                "UNIX" : 1634516298
+            }
+        },
+        {
+            "show_name" : "《Vaig Aprendre》 加泰兰语舞台剧 El petit príncep 插曲 (2021-5-22)",
+            "type" : "translation_works",
+            "file_id" : "2021-5-22-EL_PETIT_PRINCEP",
+            "post_type" : "txt",
+            "post_time" : {
+                "UNIX" : 1621703898
+            }
+        },
+        {
+            "show_name" : "De Una Vez 西语自译版与官方版对照 (2021-6-6)",
+            "type" : "translation_works",
+            "file_id" : "2021-6-6-DE_UNA_VEZ",
+            "post_type" : "txt",
+            "post_time" : {
+                "UNIX" : 1622999898
+            }
+        },
+        {
+            "show_name" : "歌词自译：《On My Way - Alan Walker》(2021-8-29)",
+            "type" : "translation_works",
+            "file_id" : "2021-8-29-ON_MY_WAY",
+            "post_type" : "txt",
+            "post_time" : {
+                "UNIX" : 1630257498
+            }
+        },
+        {
+            "show_name" : "好文分享：《Códigos》（编码）(2021-8-31)",
+            "type" : "translation_works",
+            "file_id" : "2021-8-31-CODIGOS",
+            "post_type" : "txt",
+            "post_time" : {
+                "UNIX" : 1630430298
+            }
+        },
+        {
+            "show_name" : "翻译挑战一：EL PAÍS 新闻 (2021-9-2)",
+            "type" : "translation_works",
+            "file_id" : "2021-9-2-TRAN_1",
+            "post_type" : "txt",
+            "post_time" : {
+                "UNIX" : 1630603098
+            }
+        },
+        {
+            "show_name" : "翻译挑战二：elPeriódico 新闻 (2021-9-3)",
+            "type" : "translation_works",
+            "file_id" : "2021-9-3-TRAN_2",
+            "post_type" : "txt",
+            "post_time" : {
+                "UNIX" : 1630689498
+            }
+        },
+        {
+            "show_name" : "翻译挑战三：《带上她的眼睛》—— 刘慈欣（一）(2021-9-3)",
+            "type" : "translation_works",
+            "file_id" : "2021-9-3-TRAN_3",
+            "post_type" : "txt",
+            "post_time" : {
+                "UNIX" : 1630689498
+            }
+        },
+        {
+            "show_name" : "前言 [21 - 6 - 16]",
+            "type" : "amic_retrobat",
+            "file_id" : "amicRetrobat",
+            "post_type" : "txt",
+            "post_time" : {
+                "UNIX" : 1623863898
+            }
+        }
+    ]
+    
+}
---
+++ texts/stories/2021-10-28-Azar-y-su-vida.md
@@ -0,0 +1,81 @@
+# 绝妙的好文章：《Azar y su vida》（偶然与他的人生）——我的同学
+
+这是来自于哲学课堂的作业中的一份 500 词作文，特地被哲学老师点名表扬的唯一一篇作文，拿到了满分。作者是我的一位好友，也是同学。
+
+老师给出的要求：撰写一份不低于 500 词的作文，可以是西班牙语或加泰兰语，主题围绕“人生的随机性与偶然性”。推荐观看《Match Point》电影为作文的写作添砖加瓦。
+
+我读了他的作业之后大受震撼，在他的同意下将**作文原文**（西班牙文）发布到我的博客里，我再**翻译一遍**（中文）供各位欣赏。
+
+### 作文原文（西班牙语，我的翻译版本在下面）
+
+**Azar y su vida**
+
+¿Cuándo fue cuando conocí a Azar?, ¿fue en primaria?, ¿fue en secundaria?, no me acuerdo, pasaron ya tantos años, quizás debería de dar una vuelta por los anales de mi vida para refrescar mis recuerdos sobre él.
+
+Me acuerdo que un día, un niño nuevo llegó a mi clase, se llamaba Azar y se sentaba al lado mío, desde ese entonces mi vida se volvió **inestable**.
+
+Una vez, hice un trabajo en grupo con él, nos repartimos los trabajos y prometió hacer su parte, pero al final no hizo su trabajo y nos pusieron una mala nota, desde entonces supe que Azar era **traicionero**, no hacía su trabajo y no podía confiar en él.
+
+Para ser honestos, no todo en él era malo, a veces, en los días que estaba de buen humor, nos **endulzada** el día dándonos golosinas, nos daba también algún regalo de vez en cuando, aunque no todos los niños recibían el mismo regalo, algunos recibían regalos ostentosos mientras que otros recibían regalos penosos, yo recibía regalos normales, ni ostentosos ni penosos, también recibía de vez en cuando regalos un poco ostentosos o penosos.
+
+Pero, a veces, cuando estaba de mal humor, era el mismo demonio en persona, se enfadaba enseguida, quitaba de los otros todo lo que le apetecía, si alguien le caía mal, le daba una paliza. En esos tiempos todo el mundo o le tenía miedo o le odiaba.
+
+Era bien sabido por el colegio que Azar tenía un **mal carácter**, si le pillabas de buen humor, era la persona más amable del mundo pero si le pillabas de mal humor, obtenías de todo menos risas, y yo que me sentaba al lado de él, lo sabía mejor que nadie.
+
+Nadie entendía qué pretendía hacer Azar, a veces era muy **imprevisible**, a veces hacía cosas que nadie entendía por qué lo hacía, pero él lo hacía, como por ejemplo intentar comer espaguetis por la nariz, en la hora del almuerzo siempre lo hacía, lo hizo durante 3 meses seguidos, sin faltar ningún día. Nosotros nos riamos en sus espaldas.
+
+Una vez, Azar faltó para ir al médico. Ese día, me acuerdo, fue de los más largos de mi vida.
+
+Ese día, me di cuenta que Azar hacía nuestras vidas más interesantes, a lo mejor es por el contracte entre Azar bueno y Azar malo, el bueno potenciaba nuestra alegría y el malo potenciaba nuestro odio. A lo mejor la imprevisibilidad del azar es lo que le da a la vida sentido, a lo mejor los giros en los estados de ánimo es lo que hace la vida humana interesante, a lo mejor lo ha estado haciendo todo el tiempo por nosotros, para que estemos constantemente cambiando de ánimo y, ¿sentirnos más humanos?
+
+¿Es el sentido de la vida disfrutar del azar?
+
+### 作文译版（中文，由我自行翻译）
+
+> **Azar y su vida**
+
+《偶然与他的人生》
+
+> ¿Cuándo fue cuando conocí a Azar?, ¿fue en primaria?, ¿fue en secundaria?, no me acuerdo, pasaron ya tantos años, quizás debería de dar una vuelta por los anales de mi vida para refrescar mis recuerdos sobre él.
+
+我是什么时候认识偶然的？是在小学吗？中学吗？我不记得了。已经过去了太久，也许我得将我人生的往事簿整个查阅一次才能让这段记忆再次浮现。
+
+> Me acuerdo que un día, un niño nuevo llegó a mi clase, se llamaba Azar y se sentaba al lado mío, desde ese entonces mi vida se volvió **inestable**.
+
+我记得有一天，一名新的同学来到了我的教室，他的名字叫偶然，和我组成了同桌。从那时起，我的人生变回了**动荡不定**的状态。
+
+> Una vez, hice un trabajo en grupo con él, nos repartimos los trabajos y prometió hacer su parte, pero al final no hizo su trabajo y nos pusieron una mala nota, desde entonces supe que Azar era **traicionero**, no hacía su trabajo y no podía confiar en él.
+
+有一次，我和他一起做一个小组项目，我们把各自的工作分配好后，他承诺会把他那部分做好。但最终他并没有做他的那部分作业，导致我们取得了一个坏成绩。自那时之后，我知道了偶然是**不可靠**的，他不做他该做的，我没法信任他。
+
+> Para ser honestos, no todo en él era malo, a veces, en los días que estaba de buen humor, nos **endulzada** el día dándonos golosinas, nos daba también algún regalo de vez en cuando, aunque no todos los niños recibían el mismo regalo, algunos recibían regalos ostentosos mientras que otros recibían regalos penosos, yo recibía regalos normales, ni ostentosos ni penosos, también recibía de vez en cuando regalos un poco ostentosos o penosos.
+
+实话说，他并不是处处不好。有时候，他心情愉悦，就会送我们糖吃，**带来甜蜜蜜**的一天。他还会时常送我们一些礼物，尽管每个人的礼物各不相同，有些特别美好，而有些又很差。我收到的是一些普普通通的，既不太好也不算差，只是偶尔会有一些比较好或比较差的。
+
+> Pero, a veces, cuando estaba de mal humor, era el mismo demonio en persona, se enfadaba enseguida, quitaba de los otros todo lo que le apetecía, si alguien le caía mal, le daba una paliza. En esos tiempos todo el mundo o le tenía miedo o le odiaba.
+
+但是，在某些他心情不好的时候，他就会变得跟恶魔一样坏。他会突然发怒，把别人喜欢的东西拿走。如果有人和他关系不好，还会给那人来上一拳。每当这种时刻到来，没有人不惧怕他或讨厌他。
+
+> Era bien sabido por el colegio que Azar tenía un **mal carácter**, si le pillabas de buen humor, era la persona más amable del mundo pero si le pillabas de mal humor, obtenías de todo menos risas, y yo que me sentaba al lado de él, lo sabía mejor que nadie.
+
+学校里的人都知道偶然有个**坏性格**。如果你碰巧遇见心情愉悦的他，他将会是世界上最善良的人；但如果你遇到的是一个心情糟糕的他，你就能收获到除了欢乐以外的任何事物。而我既然坐在他的旁边，自然对他最了解。
+
+> Nadie entendía qué pretendía hacer Azar, a veces era muy **imprevisible**, a veces hacía cosas que nadie entendía por qué lo hacía, pero él lo hacía, como por ejemplo intentar comer espaguetis por la nariz, en la hora del almuerzo siempre lo hacía, lo hizo durante 3 meses seguidos, sin faltar ningún día. Nosotros nos riamos en sus espaldas.
+
+没人知道偶然到底想做什么，他有时会很善变，有时会做一些令别人摸不着头脑的事情，但他依然执着地做着。比如说尝试用鼻子吃面条，每逢午餐必做此事，三个月从未间断，一日不差。我们总是在他背后嘲笑他。
+
+> Una vez, Azar faltó para ir al médico. Ese día, me acuerdo, fue de los más largos de mi vida.
+
+有一次，偶然请了病假。那一天，记忆犹新，曾是我人生中最漫长的一天。
+
+> Ese día, me di cuenta que Azar hacía nuestras vidas más interesantes, a lo mejor es por el contracte entre Azar bueno y Azar malo, el bueno potenciaba nuestra alegría y el malo potenciaba nuestro odio. A lo mejor la imprevisibilidad del azar es lo que le da a la vida sentido, a lo mejor los giros en los estados de ánimo es lo que hace la vida humana interesante, a lo mejor lo ha estado haciendo todo el tiempo por nosotros, para que estemos constantemente cambiando de ánimo y, ¿sentirnos más humanos?
+
+那一天，我发现，偶然让我们的人生更加有趣，也许是因为好偶然和坏偶然之间的反差，好偶然使我们愉悦而坏偶然让我们讨厌。也许偶然的易变性使我们的人生更有意义，也许他行为突然的变化使人生更有意思。也许他为了我们而无处不在，从而使我们不断地改变着想法，让我们更加像一个人类？
+
+> ¿Es el sentido de la vida disfrutar del azar?
+
+人生的意义就是享受偶然吗？
+
+### 完
+
+如果想看我的作业（10 月 18 日的那篇就是原文，这里指的是译版）可以在 QQ 群里讨论哦（但是，相较起来就真的没什么意思了，有点水，得分 8/10）。
