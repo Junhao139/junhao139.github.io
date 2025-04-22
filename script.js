e83f5392c93336d4d19c8d36575617e6c189109c
#JunHao <56094517+Junhao139@users.noreply.github.com>
2025-04-22 19:03:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,10 @@ let __ScriptGlobal = {
 function set_scroll_camera_blur() {
     let vert = window.scrollY;
     let size_y = window.innerHeight;
-    let proportion = Math.min(1.0, vert / size_y);
-    __RainyTextGlobal.camera.manual_focal_radius_param = proportion * 0.008;
+    if (vert < size_y) {
+        let proportion = Math.min(1.0, vert / size_y);
+        __RainyTextGlobal.camera.manual_focal_radius_param = proportion * 0.008;
+    }
 }
 
 setInterval(set_scroll_section_highlight, 200);
